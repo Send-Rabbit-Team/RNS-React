e53f66a91f3d5ae64cfc2f7969f2a36212205867
yenashin <yena5790@gachon.ac.kr>
2023-01-11 13:27:26 +0900

    [FEAT] SRT-68 구글 회원가입

--- src/views/examples/RegCompany.js
+++ src/views/examples/RegCompany.js
@@ -1,9 +1,11 @@
 import React, {useState} from "react";
-import {Button, Col, Form, FormGroup, Input, InputGroup, InputGroupAddon, InputGroupText, Row} from "reactstrap";
+import { Modal, Button, Col, Form, FormGroup, Input, InputGroup, InputGroupAddon, InputGroupText, Row, Label} from "reactstrap";
 import axios from "axios";
 
-const RegCompany = () => {
-    const [companyEmail, setCompanyEmail] = useState();
+const RegCompany = (props) => {
+    const regUrl = props.gmail == null ? "/register" : "/google/register"
+
+    const [companyEmail, setCompanyEmail] = useState(props.gmail != null ? props.gmail : "");
     const [companyPassword1, setCompanyPassword1] = useState("");
     const [companyPassword2, setCompanyPassword2] = useState("");
     const [companyManager, setCompanyManager] = useState();
@@ -15,17 +17,43 @@ const RegCompany = () => {
     const [companyPhoneCheck, setCompanyPhoneCheck] = useState("");
     const [companyBsnumCheck, setCompanyBsnumCheck] = useState("");
 
+    const [isModalOpen, setOpenModalOpen] = useState(false)
+
+    const [accessNum, setAccessNum] = useState();
+
     const authPhone = async () => {
         companyPhone == null ? window.alert("인증할 휴대폰 번호를 입력하세요") :
-            await axios.get("/", companyPhone)
+            await axios.post("/sms/send", {"to" : companyPhone})
                 .then((response) => {
-                    setCompanyPhoneCheck("has-success")
-                    window.alert("휴대폰 번호 인증에 성공했습니다")
+                    console.log(response)
+                    if (response.data.isSuccess) {
+                        setOpenModalOpen(true)
+                    } else {
+                        window.alert(response.data.message)
+                    }
                 })
                 .catch((error) => {
-                    setCompanyPhoneCheck("has-danger")
-                    window.alert("휴대폰 번호 인증에 실패했습니다")
+
                 })
+    }
+
+    const authAccessNum = async () => {
+        accessNum == null ? window.alert("인증 번호를 입력하세요") :
+            await axios.post("/sms/valid", {
+                "phoneNumber": companyPhone,
+                "authToken" : accessNum
+            }).then((response) => {
+                if (response.data.isSuccess) {
+                    setOpenModalOpen(false)
+                    setCompanyPhoneCheck("has-success")
+                    window.alert(response.data.message)
+
+                } else {
+                    setOpenModalOpen(false)
+                    setCompanyPhoneCheck("has-danger")
+                    window.alert(response.data.message)
+                }
+            })
     }
 
     const authBsnum = async () => {
@@ -61,170 +89,215 @@ const RegCompany = () => {
 
         }
         companyEmail == null ? window.alert("이메일을 입력하세요") :
-            companyPassword1 == null ? window.alert("비밀번호를 입력하세요") :
-                // companyPasswordCheck != "has-success" ? window.alert("비밀번호를 확인하세요") :
+            props.gmail == null && companyPassword1 == null ? window.alert("비밀번호를 입력하세요") :
+                // props.gmail == null && companyPasswordCheck != "has-success" ? window.alert("비밀번호를 확인하세요") :
                 companyManager == null ? window.alert("이름을 입력하세요") :
                     companyPhone == null ? window.alert("휴대폰 번호를 입력하세요") :
                         // companyPhoneCheck != "has-success" ? window.alert("휴대폰 번호를 인증하세요") :
                         companyName == null ? window.alert("회사 이름을 입력하세요") :
                             companyBsnum == null ? window.alert("사업자 번호를 입력하세요") :
                                 companyBsnumCheck != "has-success" ? window.alert("사업자 번호를 인증하세요") :
-                                await axios.post("/register", companyRegisterReq)
+                                await axios.post(regUrl, companyRegisterReq)
                                     .then((response) => {
                                         if (response.data.isSuccess == true) {
                                             window.alert(response.data.message)
-                                            window.location.replace("/login")
+                                            window.location.replace("/auth/login")
                                         } else {
                                             window.alert(response.data.message)
                                         }
                                     })
     }
     return (
-        <Form role="form">
-            {/*email*/}
-            <FormGroup>
-                <InputGroup className="input-group-alternative mb-3">
-                    <InputGroupAddon addonType="prepend">
-                        <InputGroupText>
-                            <i className="ni ni-email-83" />
-                        </InputGroupText>
-                    </InputGroupAddon>
-                    <Input
-                        placeholder="Email"
-                        type="email"
-                        autoComplete="new-email"
-                        onChange={(e)=>{setCompanyEmail(e.target.value)}}
-                    />
-                </InputGroup>
-            </FormGroup>
-
-            {/*password*/}
-            <FormGroup>
-                <InputGroup className="input-group-alternative">
-                    <InputGroupAddon addonType="prepend">
-                        <InputGroupText>
-                            <i className="ni ni-lock-circle-open" />
-                        </InputGroupText>
-                    </InputGroupAddon>
-                    <Input
-                        placeholder="Password"
-                        type="password"
-                        autoComplete="new-password"
-                        onChange={(e)=>{
-                            setCompanyPassword1(e.target.value)
-                            companyPassword1 === companyPassword2 ? setCompanyPasswordCheck("has-success") : setCompanyPasswordCheck("has-danger")
-                        }}
-                    />
-                </InputGroup>
-            </FormGroup>
-
-            {/*password check*/}
-            <FormGroup className={companyPasswordCheck}>
-                <InputGroup className="input-group-alternative">
-                    <InputGroupAddon addonType="prepend">
-                        <InputGroupText>
-                            <i className="ni ni-lock-circle-open" />
-                        </InputGroupText>
-                    </InputGroupAddon>
-                    <Input
-                        placeholder="Password Check"
-                        type="password"
-                        onChange={(e)=>{
-                            setCompanyPassword2(e.target.value)
-                            companyPassword1 === companyPassword2 ? setCompanyPasswordCheck("has-success") : setCompanyPasswordCheck("has-danger")
-                        }}
-                    />
-                </InputGroup>
-            </FormGroup>
-
-            {/*company manager name*/}
-            <FormGroup>
-                <InputGroup className="input-group-alternative mb-3">
-                    <InputGroupAddon addonType="prepend">
-                        <InputGroupText>
-                            <i className="fas fa-user" />
-                        </InputGroupText>
-                    </InputGroupAddon>
-                    <Input
-                        placeholder="Name"
-                        type="text"
-                        onChange={(e)=>{setCompanyManager(e.target.value)}}
-                    />
-                </InputGroup>
-            </FormGroup>
-
-            {/*phoneNumber*/}
-            <Row>
-                <Col xs="9">
-                    <FormGroup className={companyPhoneCheck}>
-                        <InputGroup className="input-group-alternative mb-3">
-                            <InputGroupAddon addonType="prepend">
-                                <InputGroupText>
-                                    <i className="fas fa-phone"></i>
-                                </InputGroupText>
-                            </InputGroupAddon>
-                            <Input
-                                placeholder="Phone"
-                                type="tel"
-                                onChange={(e)=>{setCompanyPhone(e.target.value)}}
-                            />
-                        </InputGroup>
-                    </FormGroup>
-                </Col>
-                <Col xs="3">
-                    <Button color="primary" outline type="button" onClick={authPhone}>
-                        인증
+        <>
+            <Modal
+                className="modal-dialog-centered"
+                isOpen={isModalOpen}
+            >
+                <div className="modal-header">
+                    <h3 className="modal-title" id="exampleModalLabel">
+                        휴대폰 인증
+                    </h3>
+                    <button
+                        aria-label="Close"
+                        className="close"
+                        data-dismiss="modal"
+                        type="button"
+                        onClick={() => setOpenModalOpen(false)}
+                    >
+                        <span aria-hidden={true}>×</span>
+                    </button>
+                </div>
+                <div className="modal-body">
+                    <Input placeholder="인증 번호를 입력하세요" type="number" onChange={(e) => {setAccessNum(e.target.value)}}/>
+                </div>
+                <div className="modal-footer">
+                    <Button
+                        color="secondary"
+                        data-dismiss="modal"
+                        type="button"
+                        onClick={() => setOpenModalOpen(false)}
+                    >
+                        Close
                     </Button>
-                </Col>
-            </Row>
-
-            {/*company name*/}
-            <FormGroup>
-                <InputGroup className="input-group-alternative mb-3">
-                    <InputGroupAddon addonType="prepend">
-                        <InputGroupText>
-                            <i className="far fa-building" />
-                        </InputGroupText>
-                    </InputGroupAddon>
-                    <Input
-                        placeholder="Company Name"
-                        type="text"
-                        onChange={(e)=>{setCompanyName(e.target.value)}}
-                    />
-                </InputGroup>
-            </FormGroup>
-
-            {/*company business number*/}
-            <Row>
-                <Col xs="9">
-                    <FormGroup className={companyBsnumCheck}>
-                        <InputGroup className="input-group-alternative mb-3">
-                            <InputGroupAddon addonType="prepend">
-                                <InputGroupText>
-                                    <i className="fas fa-briefcase" />
-                                </InputGroupText>
-                            </InputGroupAddon>
-                            <Input
-                                placeholder="Company Business Number"
-                                type="number"
-                                onChange={(e)=>{setCompanyBsnum(e.target.value)}}
-                            />
-                        </InputGroup>
-                    </FormGroup>
-                </Col>
-                <Col xs="3">
-                    <Button color="primary" outline type="button" onClick={authBsnum}>
-                        인증
+                    <Button color="primary" type="button" onClick={authAccessNum}>
+                        Save changes
                     </Button>
-                </Col>
-            </Row>
+                </div>
+            </Modal>
 
-            <div className="text-center">
-                <Button className="mt-4" color="primary" type="button" onClick={registerMember}>
-                    Create account
-                </Button>
-            </div>
-        </Form>
+            <Form role="form">
+                {/*email*/}
+                <FormGroup>
+                    <InputGroup className="input-group-alternative mb-3">
+                        <InputGroupAddon addonType="prepend">
+                            <InputGroupText>
+                                <i className="ni ni-email-83" />
+                            </InputGroupText>
+                        </InputGroupAddon>
+                        <Input
+                            readOnly={props.gmail != null ? true : false}
+                            placeholder="Email"
+                            type="email"
+                            autoComplete="new-email"
+                            onChange={(e)=>{setCompanyEmail(e.target.value)}}
+                        />
+                    </InputGroup>
+                </FormGroup>
+
+                {/*password*/}
+                {props.gmail != null ? null :
+                    <>
+                        <FormGroup>
+                            <InputGroup className="input-group-alternative">
+                                <InputGroupAddon addonType="prepend">
+                                    <InputGroupText>
+                                        <i className="ni ni-lock-circle-open" />
+                                    </InputGroupText>
+                                </InputGroupAddon>
+                                <Input
+                                    placeholder="Password"
+                                    type="password"
+                                    autoComplete="new-password"
+                                    onChange={(e)=>{
+                                        setCompanyPassword1(e.target.value)
+                                        companyPassword1 === companyPassword2 ? setCompanyPasswordCheck("has-success") : setCompanyPasswordCheck("has-danger")
+                                    }}
+                                />
+                            </InputGroup>
+                        </FormGroup>
+
+
+                        <FormGroup className={companyPasswordCheck}>
+                            <InputGroup className="input-group-alternative">
+                                <InputGroupAddon addonType="prepend">
+                                    <InputGroupText>
+                                        <i className="ni ni-lock-circle-open" />
+                                    </InputGroupText>
+                                </InputGroupAddon>
+                                <Input
+                                    placeholder="Password Check"
+                                    type="password"
+                                    onChange={(e)=>{
+                                        setCompanyPassword2(e.target.value)
+                                        companyPassword1 === companyPassword2 ? setCompanyPasswordCheck("has-success") : setCompanyPasswordCheck("has-danger")
+                                    }}
+                                />
+                            </InputGroup>
+                        </FormGroup>
+
+                    </> }
+
+
+                {/*company manager name*/}
+                <FormGroup>
+                    <InputGroup className="input-group-alternative mb-3">
+                        <InputGroupAddon addonType="prepend">
+                            <InputGroupText>
+                                <i className="fas fa-user" />
+                            </InputGroupText>
+                        </InputGroupAddon>
+                        <Input
+                            placeholder="Name"
+                            type="text"
+                            onChange={(e)=>{setCompanyManager(e.target.value)}}
+                        />
+                    </InputGroup>
+                </FormGroup>
+
+                {/*phoneNumber*/}
+                <Row>
+                    <Col xs="9">
+                        <FormGroup className={companyPhoneCheck}>
+                            <InputGroup className="input-group-alternative mb-3">
+                                <InputGroupAddon addonType="prepend">
+                                    <InputGroupText>
+                                        <i className="fas fa-phone"></i>
+                                    </InputGroupText>
+                                </InputGroupAddon>
+                                <Input
+                                    placeholder="Phone"
+                                    type="tel"
+                                    onChange={(e)=>{setCompanyPhone(e.target.value)}}
+                                />
+                            </InputGroup>
+                        </FormGroup>
+                    </Col>
+                    <Col xs="3">
+                        <Button color="primary" outline type="button" onClick={authPhone}>
+                            인증
+                        </Button>
+                    </Col>
+                </Row>
+
+                {/*company name*/}
+                <FormGroup>
+                    <InputGroup className="input-group-alternative mb-3">
+                        <InputGroupAddon addonType="prepend">
+                            <InputGroupText>
+                                <i className="far fa-building" />
+                            </InputGroupText>
+                        </InputGroupAddon>
+                        <Input
+                            placeholder="Company Name"
+                            type="text"
+                            onChange={(e)=>{setCompanyName(e.target.value)}}
+                        />
+                    </InputGroup>
+                </FormGroup>
+
+                {/*company business number*/}
+                <Row>
+                    <Col xs="9">
+                        <FormGroup className={companyBsnumCheck}>
+                            <InputGroup className="input-group-alternative mb-3">
+                                <InputGroupAddon addonType="prepend">
+                                    <InputGroupText>
+                                        <i className="fas fa-briefcase" />
+                                    </InputGroupText>
+                                </InputGroupAddon>
+                                <Input
+                                    placeholder="Company Business Number"
+                                    type="number"
+                                    onChange={(e)=>{setCompanyBsnum(e.target.value)}}
+                                />
+                            </InputGroup>
+                        </FormGroup>
+                    </Col>
+                    <Col xs="3">
+                        <Button color="primary" outline type="button" onClick={authBsnum}>
+                            인증
+                        </Button>
+                    </Col>
+                </Row>
+
+                <div className="text-center">
+                    <Button className="mt-4" color="primary" type="button" onClick={registerMember}>
+                        Create account
+                    </Button>
+                </div>
+            </Form>
+        </>
+
     );
 }
 
--- src/views/examples/Register.js
+++ src/views/examples/Register.js
@@ -17,30 +17,21 @@
 */
 
 // reactstrap components
-import React, {useState} from "react";
+import React from "react";
 import classnames from "classnames";
-import {
-  Button,
-  Card,
-  CardHeader,
-  CardBody,
-  FormGroup,
-  Form,
-  Input,
-  InputGroupAddon,
-  InputGroupText,
-  InputGroup,
-  Row,
-  Col,
-  Nav, NavItem, NavLink, TabContent, TabPane
-} from "reactstrap";
+import { Card, CardHeader, CardBody, Col, Nav, NavItem, NavLink, TabContent, TabPane, Button} from "reactstrap";
 import RegMember from "./RegMember";
 import RegCompany from "./RegCompany";
 import axios from "axios";
+// import {GoogleLogin, GoogleOAuthProvider, useGoogleLogin} from "@react-oauth/google";
+import GoogleLogin from "react-google-login";
+
+const CLIENT_ID_GOOGLE = "835228927820-80mfcdq6p90s97pqbo582f9f1s0ng935.apps.googleusercontent.com"
 
 class Register extends React.Component {
   state = {
-    tabs: 1
+    tabs: 1,
+    gmail: null
   };
   toggleNavs = (e, state, index) => {
     e.preventDefault();
@@ -48,12 +39,25 @@ class Register extends React.Component {
       [state]: index
     });
   };
+
+  // spring google(cors error)
   google = async () => {
-    await axios.get("/google")
-        .then((response)=>{
-          window.alert("구글 회원 인증에 성공했습니다")
+      await axios.get("https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=835228927820-80mfcdq6p90s97pqbo582f9f1s0ng935.apps.googleusercontent.com&scope=email&redirect_uri=http://localhost:8080/google/redirect",
+          {headers : {
+              "Access-Control-Allow-Origin":"*"
+            }})
+      // await axios.get("/google")
+          .then((response)=>{
+          console.log(response)
+          if (response.data.isSuccess) {
+            window.alert(response.data.message)
+            this.state.gmail = response.data.result["email"]
+          }
+
+
         })
         .catch((error) => {
+          console.log(error)
           window.alert("구글 회원 인증에 실패했습니다")
         })
   }
@@ -67,20 +71,46 @@ class Register extends React.Component {
                   <small>Sign up with</small>
                 </div>
                 <div className="text-center">
+
+
+                  {/*import GoogleLogin from "react-google-login";*/}
+                  <GoogleLogin
+                      clientId={CLIENT_ID_GOOGLE}
+                      onSuccess={(response) => console.log(response)}
+                      onFailure={(error) => {console.log(error)}}/>
+
+                  {/*// import {GoogleLogin, GoogleOAuthProvider, useGoogleLogin} from "@react-oauth/google";*/}
+                  {/*<GoogleOAuthProvider clientId={CLIENT_ID_GOOGLE}>*/}
+                  {/*  <GoogleLogin flow='auth-code'*/}
+                  {/*    onSuccess={async (response) => {*/}
+                  {/*      console.log(response)*/}
+                  {/*      console.log(response.credential)*/}
+                  {/*      // await axios.get("https://www.googleapis.com/oauth2/v2/userinfo?access_token=" + response.credential*/}
+                  {/*      // await axios.get("https://www.googleapis.com/oauth2/v3/userinfo",*/}
+                  {/*      //     {headers: {"Authorization" : "Bearer " + response.credential}}*/}
+                  {/*      // ).then((response) => {*/}
+                  {/*      //       console.log(response)*/}
+                  {/*      //     })*/}
+                  {/*    }}*/}
+                  {/*    onError={() => console.log("google register error")}*/}
+                  {/*  />*/}
+                  {/*</GoogleOAuthProvider>*/}
+
+                  {/*spring google(cors error)*/}
                   <Button
                       className="btn-neutral btn-icon"
                       color="default"
                       onClick={this.google}
                   >
-                <span className="btn-inner--icon">
-                  <img
-                      alt="..."
-                      src={
-                        require("../../assets/img/icons/common/google.svg")
-                            .default
-                      }
-                  />
-                </span>
+                    <span className="btn-inner--icon">
+                      <img
+                          alt="..."
+                          src={
+                            require("../../assets/img/icons/common/google.svg")
+                                .default
+                          }
+                      />
+                    </span>
                     <span className="btn-inner--text">Google</span>
                   </Button>
                 </div>
@@ -129,10 +159,10 @@ class Register extends React.Component {
                   <CardBody>
                     <TabContent activeTab={"tabs" + this.state.tabs}>
                       <TabPane tabId="tabs1">
-                        <RegMember></RegMember>
+                        <RegMember gmail={this.state.gmail}></RegMember>
                       </TabPane>
                       <TabPane tabId="tabs2">
-                        <RegCompany></RegCompany>
+                        <RegCompany gmail={this.state.gmail}></RegCompany>
                       </TabPane>
                     </TabContent>
                   </CardBody>
