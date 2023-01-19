7ee644070341ed54e1628a76452f7a9cdaff90cb
yenashin <yena5790@gachon.ac.kr>
2023-01-19 14:59:34 +0900

    [FEAT] SRT-173 이미지 첨부 기능 구현

---
+++ src/views/admin/modal/ImageModal.js
@@ -0,0 +1,94 @@
+import {Modal, Form, Button, Row, Card, CardBody, CardHeader, CardImg} from "reactstrap"
+import React, {useState} from "react";
+import {forEach} from "react-bootstrap/ElementChildren";
+
+const ImageModal = () => {
+    // const [isModal, setIsModal] = useState(true)
+
+    const [imgFile, setImgFile] = useState(null);	//파일
+    const [imgBase64, setImgBase64] = useState([]); // 파일 base64
+
+    const handleChangeFile = (event) => {
+        setImgFile(event.target.files);
+        for (var i = 0; i < event.target.files.length; i++) {
+            if (event.target.files[i]) {
+                let reader = new FileReader();
+                reader.readAsDataURL(event.target.files[i]); // 1. 파일을 읽어 버퍼에 저장합니다.
+                reader.onloadend = () => {
+                    // 2. 읽기가 완료되면 아래코드가 실행됩니다.
+                    const base64 = reader.result;
+                    if (base64) {
+                        var base64Sub = base64.toString()
+                        setImgBase64(imgBase64 => [...imgBase64, base64Sub]);
+                    }
+                }
+            }
+        }
+    }
+
+    const removeFile = (item) => {
+        setImgBase64(imgBase64.filter(e => e !== item));
+    };
+
+    return (
+        console.log(imgBase64),
+        <Modal
+            className="modal-dialog-centered"
+            size="lg"
+            isOpen={true}
+            // onClick={(e) => setIsModal(false)}
+        >
+            <div className="modal-header">
+                <h3 className="modal-title">이미지 첨부하기</h3>
+                <button
+                    className="close"
+                    data-dismiss="modal"
+                    type="button"
+                >
+                    <span aria-hidden={true}>×</span>
+                </button>
+            </div>
+            <div className="modal-body">
+                <Form>
+                    <div className="custom-file m-1">
+                        <input
+                            multiple
+                            className=" custom-file-input"
+                            type="file"
+                            onChange={handleChangeFile}
+                        ></input>
+                        <label className=" custom-file-label">
+                            Select file
+                        </label>
+                    </div>
+                </Form>
+                <Row className="mt-3">
+                    {imgBase64.map((item, index) => (
+                        <div className="col-md-3" key={index}>
+                            <Card className="m-1">
+                                <CardBody className="p-2">
+                                    <button
+                                        className="close mb-1"
+                                        data-dismiss="modal"
+                                        type="button"
+                                        onClick={(e) => removeFile(item)}
+                                    >
+                                        <span aria-hidden={true}>×</span>
+                                    </button>
+                                    <CardImg
+                                        className="d-block w-100"
+                                        src={item}/>
+                                </CardBody>
+                            </Card>
+                        </div>
+                    ))}
+                </Row>
+            </div>
+            <div className="modal-footer">
+                <Button type="button" color="primary">첨부하기</Button>
+            </div>
+        </Modal>
+    )
+
+}
+export default ImageModal
